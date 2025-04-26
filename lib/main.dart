f9e91f41e3877ff90dd43a93def7df587d426c2a
Mohamed elsaprot <elsaprot2001@gmail.com>
2025-04-26 11:26:01 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
               routerConfig: AppRouter.router,
               theme: ThemeData(
                 scaffoldBackgroundColor: Styles.scaffoldColor
-
+//
               ),
             )
         );
